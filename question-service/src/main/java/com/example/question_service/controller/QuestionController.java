d64b9c24f79deec324568e81ac950ca130376e51
Soumyodip Sutradhar <soumyodipsutra@gmail.com>
2025-11-16 20:02:23 +0530

    Initial commit

--- question-service/src/main/java/com/example/question_service/controller/QuestionController.java
+++ question-service/src/main/java/com/example/question_service/controller/QuestionController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("question")
-public class QuestionController { // this is service
+public class QuestionController {
 
     @Autowired
     QuestionService questionService;
